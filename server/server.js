149f19f1ce11558600a69dd15bac8640f9b049d0
george <nwagbarag@gmail.com>
2019-08-07 08:23:01 +0100

    fine

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require ('express');
 
-const path = require ('path');
+const path = require('path');
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('good');
+// })
 
 const publicPath = path.join(__dirname, '../public');
 
-const app = express();
 
 app.use(express.static(publicPath));
 
